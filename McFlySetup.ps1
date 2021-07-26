3163f00ce0e4b2815e5d7c3cb64fa19277c332a3
wa73rflame <87892769+wa73rflame@users.noreply.github.com>
2021-07-26 07:18:56 -0300

    Update McFlySetup.ps1

--- McFlySetup.ps1
+++ McFlySetup.ps1
@@ -1,1 +1,11 @@
-Write-Host "Hello!"
+Function McFly-ByDec {
+    $binFile = $args[0]
+    $key = $args[1]
+    for ($i = 0; $i -lt $binFile.Lenght; $i++){
+        if ($binFile[$i] >= $key){
+            $binFile[$i] = $binFile[$i] - $key
+        }else {
+            $binFile[$i] = (255 + $binFile[$i]) - $key
+        }
+    }
+}
