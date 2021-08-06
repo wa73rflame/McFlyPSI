d7191f862ed28cc9fcad0f70616baf72ea3f4af5
wa73rflame <87892769+wa73rflame@users.noreply.github.com>
2021-08-06 12:58:42 -0300

    Update McFlySetup.ps1

--- McFlySetup.ps1
+++ McFlySetup.ps1
@@ -1,11 +1,56 @@
-Function McFly-ByDec {
-    $binFile = $args[0]
-    $key = $args[1]
-    for ($i = 0; $i -lt $binFile.Lenght; $i++){
-        if ($binFile[$i] -ge $key){
-            $binFile[$i] = $binFile[$i] - $key
-        }else {
-            $binFile[$i] = (255 + $binFile[$i]) - $key
+Function McFly-BVE {
+    $byar = $Args[0]
+    $key = $Args[1]
+    [byte[]] $keymap = $null
+    while($key -ne 1){
+        $keymap += $key
+        $key = $key - 1
+    }
+    while($key -ne $keymap[0]){
+        $keymap += $key
+        $key = $key + 1
+    }
+    $kmc = 0
+    for($i = 0; $i -lt $byar.length; $i++) {
+        $wb = $byar[$i]
+        $wb = $wb + $keymap[$kmc]
+        if($wb -gt 255) {
+            $wb = $wb - 256
+        }
+        $byar[$i] = $wb
+        if($kmc -eq $keymap.length - 1){
+            $kmc = 0
+        }else{
+            $kmc = $kmc + 1
         }
     }
+    return $byar
+}
+Function McFly-BVD {
+    $byar = $Args[0]
+    $key = $Args[1]
+    [byte[]] $keymap = $null
+    while($key -ne 1){
+        $keymap += $key
+        $key = $key - 1
+    }
+    while($key -ne $keymap[0]){
+        $keymap += $key
+        $key = $key + 1
+    }
+    $kmc = 0
+    for($i = 0; $i -lt $byar.length; $i++) {
+        $wb = $byar[$i]
+        $wb = $wb - $keymap[$kmc]
+        if($wb -lt 0) {
+            $wb = $wb + 256
+        }
+        $byar[$i] = $wb
+        if($kmc -eq $keymap.length - 1){
+            $kmc = 0
+        }else{
+            $kmc = $kmc + 1
+        }
+    }
+    return $byar
 }
